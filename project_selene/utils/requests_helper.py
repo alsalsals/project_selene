1b06ef6a52422d90e0854ce12de5afde3c16bf06
Alsu Fayzullina <alsufayzi@gmail.com>
2024-06-20 22:33:25 +0300

    refactoring

---
+++ project_selene/utils/requests_helper.py
@@ -0,0 +1,8 @@
+import json
+
+
+def load_schema(filepath):
+    with open('filepath') as file:
+        schema = json.load(file)
+
+
--- tests/api/test_reqres.py
+++ tests/api/test_reqres.py
@@ -1,18 +1,17 @@
 import json
-
 import requests
 from requests import Response
 import jsonschema
+from project_selene.utils.requests_helper import load_schema
 
 
 def test_single_user():
     """ Query the user and get code status 200 """
     url = 'https://reqres.in/api/users/2'
+    schema = load_schema('get_single_user.json')
+
     result: Response = requests.get(url)
 
-    with open('get_single_user.json') as file:
-        schema = json.load(file)
-        jsonschema.validate(result.json(), schema)
-
     assert result.status_code == 200
+    jsonschema.validate(result.json(), schema)
 
