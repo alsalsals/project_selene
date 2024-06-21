c19ed2c8415de5f5e683c7d944c85ace3cfd1352
Alsu Fayzullina <alsufayzi@gmail.com>
2024-06-21 07:03:47 +0300

    add tests: test_single_user_id and test_list_of_users_pagination

--- tests/api/test_reqres.py
+++ tests/api/test_reqres.py
@@ -1,4 +1,6 @@
 import json
+
+import pytest
 import requests
 from requests import Response
 import jsonschema
@@ -15,3 +17,25 @@ def test_single_user():
     assert result.status_code == 200
     jsonschema.validate(result.json(), schema)
 
+
+@pytest.mark.parametrize('id_', [1, 2, 3])
+def test_single_user_id(id_):
+    """  """
+    url = f'https://reqres.in/api/users/{id_}'
+
+    result: Response = requests.get(url)
+
+    print(result.json()['data']['id'] == id_)
+    assert result.json()['data']['id'] == id_
+
+
+def test_list_of_users_pagination():
+    """  """
+    page = 2
+    per_page = 6
+    url = f'https://reqres.in/api/users'
+    result = requests.get(url, params={"page": page, "per_page": per_page})
+
+    assert result.json()['page'] == page
+    assert result.json()['per_page'] == per_page
+    assert len(result.json()['data']) == per_page
