21f5ac60158249aa339076a4e90dfd1bcf43ab6c
Alsu Fayzullina <alsufayzi@gmail.com>
2024-06-23 00:48:21 +0300

    refactoring

---
+++ tests/api/test_demowebshop.py
@@ -0,0 +1,42 @@
+import json
+
+import allure
+import requests
+from allure_commons.types import AttachmentType
+from selene import browser, have
+
+URL_demowebshop = 'https://demowebshop.tricentis.com'
+LOGIN = "alal@gmail.com"
+
+
+def test_login_api():
+    ''' Verify successful user login via API '''
+    result = requests.post(
+        url=URL_demowebshop+'/login',
+        data={"Email": LOGIN, "Password": "Anime312", "RememberMe": False},
+        allow_redirects=False
+    )
+
+    allure.attach(
+        body=result.text,
+        name="Response",
+        attachment_type=AttachmentType.TEXT,
+        extension='txt')
+    allure.attach(
+        body=json.dumps(result.json(), indent=4, ensure_ascii=True),
+        name="Response",
+        attachment_type=AttachmentType.JSON,
+        extension='json'
+    )
+
+    print(result.status_code)
+    print(result.text)
+    print(result.cookies)
+    cookie = result.cookies.get('NOPCOMMERCE.AUTH')
+
+    browser.open(URL_demowebshop)
+    browser.driver.add_cookie({'name': 'NOPCOMMERCE.AUTH', 'value': cookie})
+    browser.open(URL_demowebshop)
+
+    with allure.step("Verify successful authorization"):
+        browser.element(".account").should(have.text(LOGIN))
--- tests/api/test_reqres.py
+++ tests/api/test_reqres.py
@@ -1,5 +1,3 @@
-import json
-
 import pytest
 import requests
 from requests import Response
@@ -20,24 +18,26 @@ def test_single_user():
 
 @pytest.mark.parametrize('id_', [1, 2, 3])
 def test_single_user_id(id_):
-    """ Query the user id and get user id """
+    """ Query the user and get user id """
     url = f'https://reqres.in/api/users/{id_}'
 
     result: Response = requests.get(url)
 
-    print(result.json()['data']['id'] == id_)
+    # print(result.json()['data']['id'] == id_)
     assert result.json()['data']['id'] == id_
 
 
 def test_list_of_users_pagination():
-    """  """
+    """ The endpoint returns correct data for the given page number """
     page = 2
     per_page = 6
     url = f'https://reqres.in/api/users'
+    token = "request.post(url, json={'user': 'Alex'}).json()[token]"
+
     result: Response = requests.get(
         url=url,
         params={"page": page, "per_page": per_page},
-        headers={"Content-Type": "multipart/form-data"},
+        headers={"Content-Type": "multipart/form-data", "Authorization": f"Bearer {token}"},
         json={},
         cookies={}
     )
